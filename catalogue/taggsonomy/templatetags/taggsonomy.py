d79e15bdc6be97f08739e558f55b50952a0132fd
Gabriel Niebler <gabriel.niebler@suse.de>
2019-02-24 23:27:00 +0100

    Refactor: Pull out code to get tagset for tagged object

--- catalogue/taggsonomy/templatetags/taggsonomy.py
+++ catalogue/taggsonomy/templatetags/taggsonomy.py
@@ -4,9 +4,15 @@ from django.contrib.contenttypes.models import ContentType
 from taggsonomy.models import TagSet
 
 
+def get_tagset_for_object(object_):
+    content_type = ContentType.objects.get_for_model(object_)
+    tagset, _ = TagSet.objects.get_or_create(content_type=content_type,
+                                             object_id=object_.id)
+    return tagset
+
 register = template.Library()
 
 @register.inclusion_tag('taggsonomy/tags.html')
 def tags(tagged_object):
-    tagset, _ = TagSet.objects.get_or_create(content_type=ContentType.objects.get_for_model(tagged_object), object_id=tagged_object.id)
+    tagset = get_tagset_for_object(tagged_object)
     return {'tags' :  tagset.all()}
