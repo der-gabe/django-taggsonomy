bfd7aba13e1597afe2bc808ad817e1b68863cb0f
Gabriel Niebler <gabriel.niebler@suse.com>
2019-03-15 16:39:29 +0100

    Implement automatic TagSet deletion
    … still a bit clunky, could stand some refactoring.
    
    Signal import should be moved to AppConfig.ready(), when available.

---
+++ catalogue/taggsonomy/signals.py
@@ -0,0 +1,19 @@
+from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import pre_delete
+from django.dispatch import receiver
+
+from .models import TagSet
+from .utils import get_tagset_for_object
+
+
+@receiver(pre_delete)
+def delete_tagset(sender, **kwargs):
+    instance = kwargs.get('instance')
+    if instance:
+        content_type = ContentType.objects.get_for_model(instance)
+        if TagSet.objects\
+                 .filter(content_type=content_type,
+                         object_id=instance.id)\
+                 .exists():
+            tagset = get_tagset_for_object(instance)
+            tagset.delete()
