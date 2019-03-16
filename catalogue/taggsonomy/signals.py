f9b24d8686958d38da2a723a7f01f95b30a19a97
Gabriel Niebler <gabriel.niebler@suse.com>
2019-03-16 11:00:24 +0100

    Introduce new `get_tagset_for_object` utility function
    which may also return None, if the object does not have an associated tag set yet.

--- catalogue/taggsonomy/signals.py
+++ catalogue/taggsonomy/signals.py
@@ -3,7 +3,8 @@ from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 
 from .models import TagSet
-from .utils import get_or_create_tagset_for_object
+from .utils import (get_tagset_for_object,
+                    get_or_create_tagset_for_object)
 
 
 @receiver(pre_delete)
@@ -11,9 +12,6 @@ def delete_tagset(sender, **kwargs):
     instance = kwargs.get('instance')
     if instance:
         content_type = ContentType.objects.get_for_model(instance)
-        if TagSet.objects\
-                 .filter(content_type=content_type,
-                         object_id=instance.id)\
-                 .exists():
-            tagset = get_or_create_tagset_for_object(instance)
+        tagset = get_tagset_for_object(instance)
+        if tagset:
             tagset.delete()
--- catalogue/taggsonomy/utils.py
+++ catalogue/taggsonomy/utils.py
@@ -3,6 +3,15 @@ from django.contrib.contenttypes.models import ContentType
 from .models import TagSet
 
 
+def get_tagset_for_object(object_):
+    content_type = ContentType.objects.get_for_model(object_)
+    try:
+        tagset = TagSet.objects.get(content_type=content_type,
+                                    object_id=object_.id)
+    except TagSet.DoesNotExist:
+        tagset = None
+    return tagset
+
 def get_or_create_tagset_for_object(object_):
     content_type = ContentType.objects.get_for_model(object_)
     tagset, _ = TagSet.objects.get_or_create(content_type=content_type,
