cfa99560111c214e5676e3bf04a66321a0102c08
Gabriel Niebler <gabriel.niebler@suse.de>
2020-02-25 22:09:11 +0100

    Implement new way to pass formattable URL template string to templatetags

--- templatetags/taggsonomy.py
+++ templatetags/taggsonomy.py
@@ -8,32 +8,42 @@ from taggsonomy.utils import get_tag_object, get_or_create_tagset_for_object
 register = template.Library()
 
 @register.inclusion_tag('taggsonomy/tag.html')
-def tag(tag, tag_context=None, removable=False):
+def tag(tag, tag_context=None, removable=False, url=''):
     """
     Templatetag to render a single tag
 
     :param tag: The tag to render
     :type tag: Tag, str or int
-    :param removable: `True` if the tag should be rendered with a little 'x', hyperlinked to a removal URL
-    :type removable: bool, optional
     :param tag_context: The context (object) from which to remove the tag, e.g. a TagSet.
                         Only relevant iff removable=True - then it's needed to determine the removal URL
     :type tag_context: TagSet, optional
+    :param removable: `True` if the tag should be rendered with a little 'x', hyperlinked to a removal URL
+    :type removable: bool, optional
+    :param url: An (optionally formattable) string to serve as the tag's target URL
+    :type url: str, optional
     """
     template_context = {'tag': get_tag_object(tag)}
     if removable and tag_context:
         if isinstance(tag_context, TagSet):
             tagset = tag_context
             template_context.update({'removal_url': reverse('taggsonomy:remove-tag', args=(tagset.id, tag.id))})
+    if url:
+        url_formatter = {
+            'tag': tag,
+            'tag_id': tag.id,
+            'tag_name': tag.name,
+            'tag_color': tag.color,
+        }
+        template_context.update({'url': url.format(**url_formatter)})
     return template_context
 
 @register.inclusion_tag('taggsonomy/tags.html')
-def tags(tagged_object, url=None):
+def tags(tagged_object, url=''):
     tagset = get_or_create_tagset_for_object(tagged_object)
     return {'tags' :  tagset.all(), 'url': url}
 
 @register.inclusion_tag('taggsonomy/active_tags.html')
-def active_tags(tagged_object, url=None):
+def active_tags(tagged_object, url=''):
     tagset = get_or_create_tagset_for_object(tagged_object)
     return {'tagset' :  tagset, 'url': url}
 
@@ -45,5 +55,5 @@ def add_tags_form(tagged_object):
     return {'tags': tags, 'tagset' :  tagset}
 
 @register.inclusion_tag('taggsonomy/tag_manager.html')
-def tag_manager(tagged_object, url=None):
+def tag_manager(tagged_object, url=''):
     return {'object': tagged_object, 'url': url}
